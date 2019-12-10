77d34037af5787c79a5af5a02ccf8d5bfd665048
mickmed <mick2090@gmail.com>
2019-12-10 14:49:49 -0500

    post association

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const PORT = process.env.PORT || 3000
+const bodyparser = require('body-parser')
 
 const app = express()
+app.use(bodyparser.json())
 const { Student, Teacher, Classroom } = require('./models')
 
 app.listen(PORT, () => {
@@ -18,13 +20,42 @@ app.get('/teachers', async(req,res)=>{
 app.get('/teachers/:id/students', async(req, res)=> {
     const teacher = await Teacher.findAll({
         where: {id: req.params.id},
-        through:Classroom,
+        
         include:[{
             model:Student,
             
-            
+        
         }]
     })
 
     res.send({teacher})
+})
+app.get('/students/:id/teachers', async(req, res)=> {
+    const student = await Student.findAll({
+        where: {id: req.params.id},
+        
+        include:[{
+            model:Teacher,
+            
+        
+        }]
+    })
+
+    res.send({student})
+})
+
+
+app.get('/students',async(req,res)=>{
+    const students = await Student.findAll()
+    res.send(students)
+})
+app.post('/teachers/:id/students', async(req, res)=>{
+    console.log(req.body)
+    const teacher = await Teacher.findAll({
+        where:{id: req.params.id}
+    })
+    
+    const student = await Student.create(req.body)
+    const classroom = await Classroom.create({teacher_id:req.params.id, student_id:student.id})
+    res.send({student:student, classroom:classroom})
 })
